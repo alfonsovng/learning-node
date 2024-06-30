e485c5c372f8a329f9d265ebf8a3e76a96521467
Alfonso <adasilva@tetrao.eu>
2024-06-30 09:00:29 +0200

    Afegit mongodb

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# nodejs-crud-api
+#   
 
 Tutorial: https://www.youtube.com/watch?v=_7UQPve99r4
 
@@ -6,7 +6,11 @@ Tutorial: https://www.youtube.com/watch?v=_7UQPve99r4
 
 * https://www.npmjs.com/package/express
 * https://www.npmjs.com/package/nodemon en mode development
+* https://www.npmjs.com/package/mongo
+* https://www.npmjs.com/package/mongoose
+* https://www.npmjs.com/package/config
 
 ## Extensions de Visual Code
 
 * [Thunder Client](https://marketplace.visualstudio.com/items?itemName=rangav.vscode-thunder-client)
+* [MongoDB for VS Code](https://marketplace.visualstudio.com/items?itemName=mongodb.mongodb-vscode)
--- index.js
+++ index.js
@@ -1,10 +1,53 @@
+const config = require('config');
 const express = require('express')
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
 const app = express()
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+// permet fer servir JSON
+app.use(express.json())
 
 app.get('/', (req, res) => {
    res.send('Hello World')
+});
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+app.get('/api/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+ 
+
+mongoose.connect(config.get('mongodb.uri'))
+.then(() => {
+    console.log('Connected to mongodb database!');
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000")
+    });
 })
+.catch(() => {
+    console.log('Connection to mongodb database failed!')
+    }
+);
