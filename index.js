3639e136a393e3ecbf1ec8c004ab1e21ac08593e
Alfonso <adasilva@tetrao.eu>
2024-07-01 16:53:45 +0200

    Afegides routes i controladors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const config = require('config');
 const express = require('express')
 const mongoose = require('mongoose');
-const Product = require('./models/product.model.js');
+const productRoute = require('./routes/product.route.js')
+
 const app = express()
 
 // permet fer servir JSON
@@ -10,68 +11,13 @@ app.use(express.json());
 // permet interpretar dades enviades a la URL
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-   res.send('Hello World')
-});
+// routes
+app.use("/api/products", productRoute);
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-});
 
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-});
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-});
-
-app.put('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        
-        if(!product) {
-            res.status(404).json({message: "Product not found"});
-        } else {
-            //product conté la versió anterior
-            const updatedProduct = await Product.findById(id);
-            res.status(200).json(updatedProduct);
-        }
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-});
-
-app.delete('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndDelete(id);
-        if(!product) {
-            res.status(404).json({message: "Product not found"});
-        } else {
-            res.status(200).json({message: `Product with id ${id} deleted successfully`});
-        }
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-});
+// app.get('/', (req, res) => {
+//    res.send('Hello World')
+// });
 
 mongoose.connect(config.get('mongodb.uri'))
 .then(() => {
